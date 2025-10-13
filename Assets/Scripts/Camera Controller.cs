b042626ce46c1aa10e614bbc9af149e55d921f20
ZachAttackexe <125323809+ZachAttackexe@users.noreply.github.com>
2025-10-13 11:59:31 -0400

    Camera Controller Script and basic map elements
    Camera controller script created and attached to main camera. Basic map elements made, mainly just basic borders and map backdrop to help set up shapes

---
+++ Assets/Scripts/Camera Controller.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform target;      
+    public float smoothSpeed = 0.125f; 
+    public Vector3 offset;         
+
+    void LateUpdate()
+    {
+        if (target == null)
+        {
+            Debug.LogWarning("No target assigned!");
+            return;
+        }
+
+        Vector3 desiredPosition = target.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+
+        smoothedPosition.z = transform.position.z;
+        transform.position = smoothedPosition;
+    }
+}
