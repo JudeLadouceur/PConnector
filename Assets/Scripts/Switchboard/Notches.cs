792be611e8bb762108998014f62fc856b9d1d22f
JudeLadouceur <jude.m.ladouceur@gmail.com>
2025-11-13 15:37:10 -0500

    Context calls are in!

--- Assets/Scripts/Switchboard/Notches.cs
+++ Assets/Scripts/Switchboard/Notches.cs
@@ -4,21 +4,15 @@ using UnityEngine;
 
 public class Notches : MonoBehaviour
 {
-    private DrawLine lm;
     [HideInInspector]
     public bool isOccupied = false;
 
     public SO_Character assignedCharacter;
 
-    private void Start()
-    {
-        lm = GameObject.Find("LineManager").GetComponent<DrawLine>();
-    }
-
     private void OnMouseDown()
     {
         if (isOccupied) return;
-        lm.SelectPoint(gameObject);
+        LineManager.instance.SelectPoint(gameObject);
         isOccupied = true;
     }
 }
