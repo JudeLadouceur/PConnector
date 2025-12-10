03eaae11434fb985449d8557d093e3bc6c3a7bc3
Naitoshadou <117182054+Naitoshadou@users.noreply.github.com>
2025-12-10 13:39:40 -0500

    Fix for free move if talk and achieve bug

--- Assets/Scripts/Dialogue/DialogueManager.cs
+++ Assets/Scripts/Dialogue/DialogueManager.cs
@@ -163,6 +163,7 @@ public class DialogueManager : MonoBehaviour
         if (SceneManager.GetActiveScene().name != "Switchboard")
         {
             MovementScript.instance.Funny = false;
+            MovementScript.instance.canToggle = true;
             return;
         }
 
