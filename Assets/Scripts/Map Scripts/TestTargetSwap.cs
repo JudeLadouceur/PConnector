2a0bbe67de3b26fe6c8fb07483a585d41c4532f2
Naitoshadou <117182054+Naitoshadou@users.noreply.github.com>
2026-01-17 13:58:24 -0500

    Tutorial objectives UI added

--- Assets/Scripts/Map Scripts/TestTargetSwap.cs
+++ Assets/Scripts/Map Scripts/TestTargetSwap.cs
@@ -5,12 +5,26 @@ using UnityEngine;
 public class TestTargetSwap : MonoBehaviour
 {
     public Transform[] targets;
+    public string[] targetText;
     public ArrowPoint arrow;
+    public TMPro.TextMeshProUGUI tmp;
+
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Alpha1)){
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
             arrow.ChangeTarget(targets[0]);
-        } else if (Input.GetKeyDown(KeyCode.Alpha2)){ arrow.ChangeTarget(targets[1]); }
-        else if (Input.GetKeyDown(KeyCode.Alpha3)){ arrow.ChangeTarget(targets[2]); }
+            tmp.text = targetText[0];
+        } 
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            arrow.ChangeTarget(targets[1]);
+            tmp.text = targetText[1];
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            arrow.ChangeTarget(targets[2]);
+            tmp.text = targetText[2];
+        }
     }
 }
