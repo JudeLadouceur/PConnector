2cc6ccb934c36b4f5b7a75b75d81069a39694da0
Naitoshadou <117182054+Naitoshadou@users.noreply.github.com>
2026-01-17 14:39:43 -0500

    Lockout functionality added to testing

--- Assets/Scripts/Map Scripts/TestTargetSwap.cs
+++ Assets/Scripts/Map Scripts/TestTargetSwap.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class TestTargetSwap : MonoBehaviour
@@ -8,23 +9,77 @@ public class TestTargetSwap : MonoBehaviour
     public string[] targetText;
     public ArrowPoint arrow;
     public TMPro.TextMeshProUGUI tmp;
+    public MovementScript player;
+    public bool lockoutObj = false;
+    public int currentObjective = 0;
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Alpha1))
         {
-            arrow.ChangeTarget(targets[0]);
-            tmp.text = targetText[0];
+            SetObjective(0);
+            EndLockout();
         } 
         else if (Input.GetKeyDown(KeyCode.Alpha2))
         {
-            arrow.ChangeTarget(targets[1]);
-            tmp.text = targetText[1];
+            SetObjective(1);
+            EndLockout();
         }
         else if (Input.GetKeyDown(KeyCode.Alpha3))
         {
-            arrow.ChangeTarget(targets[2]);
-            tmp.text = targetText[2];
+            SetObjective(2);
+            EndLockout();
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            SetObjective(3);
+            StartLockout();
+        }
+    }
+
+    public void DisableArrow()
+    {
+        arrow.gameObject.SetActive(false);
+    }
+    public void EnableArrow()
+    {
+        arrow.gameObject.SetActive(true);
+    }
+
+    public void StartLockout()
+    {
+        lockoutObj = true;
+        player.Funny = true;
+    }
+
+    public void EndLockout()
+    {
+        if (lockoutObj)
+        {
+            lockoutObj = false;
+            player.Funny = false;
+        }
+        if(currentObjective == targets.Count() - 1)
+        {
+            SetObjective(0);
+        }
+        
+    }
+
+    public void SetObjective(int num)
+    {
+        currentObjective = num;
+        if (targetText[num] != null)
+        {
+            tmp.text = targetText[num];
+        }
+        if (targets[num] != null)
+        {
+            EnableArrow();
+            arrow.ChangeTarget(targets[num]);
+        } else
+        {
+            DisableArrow();
         }
     }
 }
