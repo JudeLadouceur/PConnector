780d9df8831488e323859f9c5fbe051ee5a462fb
Naitoshadou <117182054+Naitoshadou@users.noreply.github.com>
2026-02-05 12:10:01 -0500

    Gate function hooked up

--- Assets/Scripts/TutorialInteract.cs
+++ Assets/Scripts/TutorialInteract.cs
@@ -7,6 +7,7 @@ public class TutorialInteract : Interactables
     public Transform moveableObject;
     public Vector3 moveToRelative;
     public int stepIndex;
+    public Animator anim;
 
 
     public override void Interact()
@@ -14,6 +15,7 @@ public class TutorialInteract : Interactables
         base.Interact();
         Vector3 moveTarget = moveableObject.position + moveToRelative;
         moveableObject.position = moveTarget;
+        anim.SetBool("Triggered", true);
         if(TestTargetSwap.instance!=null && TestTargetSwap.instance.currentStep == stepIndex)
         {
             TestTargetSwap.instance.AttemptProgress(stepIndex);
