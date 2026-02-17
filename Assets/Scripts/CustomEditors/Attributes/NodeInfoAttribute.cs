5307db74f9d9a553d9dc572ae53f27cd66a3a0d2
JudeLadouceur <jude.m.ladouceur@gmail.com>
2026-02-17 12:26:35 -0500

    Added a return to main menu node

--- Assets/Scripts/CustomEditors/Attributes/NodeInfoAttribute.cs
+++ Assets/Scripts/CustomEditors/Attributes/NodeInfoAttribute.cs
@@ -20,13 +20,13 @@ public class NodeInfoAttribute : Attribute
     public bool hasMultipleFlowOutputs => m_hasMultipleFlowOutputs;
 
 
-    public NodeInfoAttribute(string title, string menuItem = "", bool hasFlowInput = true, bool hasMultipleFlowInputs = false, bool hasFlowOuput = true, bool hasMultipleFlowOutputs = false)
+    public NodeInfoAttribute(string title, string menuItem = "", bool hasFlowInput = true, bool hasMultipleFlowInputs = false, bool hasFlowOutput = true, bool hasMultipleFlowOutputs = false)
     {
         m_nodeTitle = title;
         m_menuItem = menuItem;
         m_hasFlowInput = hasFlowInput;
         m_hasMultipleFlowInputs = hasMultipleFlowInputs;
-        m_hasFlowOutput = hasFlowOuput;
+        m_hasFlowOutput = hasFlowOutput;
         m_hasMultipleFlowOutputs = hasMultipleFlowOutputs;
     }
 }
