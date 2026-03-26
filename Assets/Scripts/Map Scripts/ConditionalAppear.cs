3a598c9e49a87ab3e90ed6ce0c2bf89ed0ac1db9
JudeLadouceur <jude.m.ladouceur@gmail.com>
2026-03-26 15:08:38 -0400

    made a script to have things appear conditionally in the overworld

---
+++ Assets/Scripts/Map Scripts/ConditionalAppear.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static OverworldDialogue;
+
+public class ConditionalAppear : MonoBehaviour
+{
+    [Serializable]
+    public class VariableInfo
+    {
+        public DialogueVar variable;
+        public int value;
+    }
+
+    public VariableInfo[] Variables;
+
+    private void Awake()
+    {
+        VariableInfo variableInfo = null;
+
+        for (int i = 0; i < Variables.Length; i++)
+        {
+            Debug.Log("Checking option: " + i);
+
+            //If the variable is incorrect stop checking and disappear
+            if (VariableManager.instance.flags[variableInfo.variable] != variableInfo.value)
+            {
+                Debug.Log("variableInfo " + variableInfo.variable + " was " + VariableManager.instance.flags[variableInfo.variable] + ", but was looking for " + variableInfo.value);
+
+                gameObject.SetActive(false);
+
+                return;
+            }
+            //If the variable is correct, keep going
+            else
+            {
+                Debug.Log("variableInfo " + variableInfo.variable + " is the correct value.");
+            }
+        }
+    }
+}
